c33b20ab59b73122b2ad3ecccb67325410ef9559
Zulfiqar Ali <8zali9@gmail.com>
2024-03-21 01:49:12 +0500

    beautified app

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import './header.css'
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { TiAdjustBrightness } from "react-icons/ti";
 
 export default function Header() {
+  const [toggle, setToggle] = useState(false)
+
+  const handleBtnBg = () => {
+    const btn = document.querySelector(".toggle-btn")
+    btn.addEventListener('click', function() {
+      btn.classList.add('btn-bg')
+      setTimeout(function() {
+        btn.classList.remove('btn-bg')
+      }, 300)
+    })
+  }
+
+  const handleToggle = () => {
+    setToggle(!toggle)
+    
+    if (toggle) {
+      document.body.classList.add("bright-bg")
+      document.body.classList.remove("dark-bg")
+    } else {
+      document.body.classList.add("dark-bg")
+      document.body.classList.remove("white-bg")
+    }
+
+    handleBtnBg ()
+  }
+
   return (
     <div className='header'>
         <p className="heading">القرآن</p>
+        <button onClick={handleToggle} className='toggle-btn'>
+          {
+            toggle ? <BsFillMoonStarsFill /> : <TiAdjustBrightness />
+          }
+        </button>
     </div>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,34 +1,42 @@
 "use client"
 
 import { useEffect, useState } from "react";
+import { RiDoubleQuotesL } from "react-icons/ri";
+import { IoReloadCircleSharp } from "react-icons/io5";
 
 export default function Home() {
   const [verse, setVerse] = useState("...")
   const [verseNumber, setVerseNumber] = useState("...")
   const [chapter, setChapter] = useState("")
 
-  useEffect(() => {
+  const getVerse = async () => {
     const anyVerse = Math.floor(Math.random() * 6348) + 1;
-    async function getVerse() {
-      const response = await fetch(`https://api.alquran.cloud/v1/ayah/${anyVerse}/en.asad`)
+    const response = await fetch(`https://api.alquran.cloud/v1/ayah/${anyVerse}/en.asad`)
 
-      const verseData = await response.json()
-      setVerse(verseData.data.text)
-      setChapter(verseData.data.surah.englishName)
-      setVerseNumber(verseData.data.numberInSurah)
-    }
+    const verseData = await response.json()
+    setVerse(verseData.data.text)
+    setChapter(verseData.data.surah.englishName)
+    setVerseNumber(verseData.data.numberInSurah)
+  }
 
+  const handleGetVerse = () => {
+    getVerse()
+  }
+
+  useEffect(() => {    
     getVerse()
   }, [])
+
   return (
     <main className="main">
-      <p className="verse">"{verse}"</p>
+      <RiDoubleQuotesL fontSize="25px"/>
+      <p className="verse">{verse}</p>
       <div className="verse-details">
         <p className="verse-details-div-content">Chapter: </p>
         <div className="verse-details-div">
           <p className="verse-details-div-content">{chapter} [{verseNumber}]</p>
         </div>
-
+        <button onClick={handleGetVerse} className="toggle-btn reload"><IoReloadCircleSharp /></button>
       </div>
     </main>
   );
