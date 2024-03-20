791435afb3e42f8d476c12910801056e5f97a2d1
Zulfiqar Ali <8zali9@gmail.com>
2024-03-20 09:58:22 +0500

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [verse, setVerse] = useState("...")
+  const [verseNumber, setVerseNumber] = useState("...")
+  const [chapter, setChapter] = useState("")
+
+  useEffect(() => {
+    const anyVerse = Math.floor(Math.random() * 6348) + 1;
+    async function getVerse() {
+      const response = await fetch(`http://api.alquran.cloud/v1/ayah/${anyVerse}/en.asad`)
+
+      const verseData = await response.json()
+      setVerse(verseData.data.text)
+      setChapter(verseData.data.surah.englishName)
+      setVerseNumber(verseData.data.numberInSurah)
+    }
+
+    getVerse()
+  }, [])
+  return (
+    <main className="main">
+      <p className="verse">"{verse}"</p>
+      <div className="verse-details">
+        <p className="verse-details-div-content">Chapter: </p>
+        <div className="verse-details-div">
+          <p className="verse-details-div-content">{chapter} [{verseNumber}]</p>
+        </div>
+
+      </div>
+    </main>
+  );
+}
